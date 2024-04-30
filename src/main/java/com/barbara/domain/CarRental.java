ea04e13570e2fa0186343a4acd33b149dd684197
BarbaraKarvalho <barbara_silvac@hotmail.com>
2024-04-30 20:26:49 -0300

    Initial program with models

---
+++ src/main/java/com/barbara/domain/CarRental.java
@@ -0,0 +1,13 @@
+package com.barbara.domain;
+
+import java.time.LocalDateTime;
+
+public class CarRental {
+
+    private LocalDateTime start;
+    private LocalDateTime finish;
+
+    private Vehicle vehicle;
+
+
+}
---
+++ src/main/java/com/barbara/domain/Vehicle.java
@@ -0,0 +1,23 @@
+package com.barbara.domain;
+
+public class Vehicle {
+
+    private String model;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+
+}
